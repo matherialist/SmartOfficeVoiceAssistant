40c5071835c926b0c21631ce09497efb5d42d528
matherialist <lagrange@phystech.edu>
2020-02-25 23:07:23 +0300

    Refactored

--- src/ActionClassifer.py
+++ src/ActionClassifer.py
@@ -18,47 +18,22 @@ class ActionClassifier:
             self.intents_label_encoder = pickle.load(handle)
         self.model = JointBertModel.load(load_folder_path, sess)
 
-    @staticmethod
-    def convert_to_slots(slots_arr, no_class_tag='O', begin_prefix='B-', in_prefix='I-'):
-        previous = None
-        slots = []
-        start = -1
-        end = -1
-
-        def add(name, start, end):
-            if end < start:
-                end = start
-            slots.append((name, start, end))
-
-        for i, slot in enumerate(slots_arr):
-            if slot == no_class_tag:
-                current = None
-                if previous is not None:
-                    add(previous, start, end)
-            if slot.startswith(begin_prefix):
-                current = slot[len(begin_prefix):]
-                if previous is not None:
-                    add(previous, start, end)
-                start = i
-            elif slot.startswith(in_prefix):
-                current = slot[len(in_prefix):]
-                if current != previous:
-                    if previous is not None:
-                        add(previous, start, end)
-                    for j, sl in enumerate(slots):
-                        if current == sl[0]:
-                            sl = list(sl)
-                            sl[2] = i
-                            slots[j] = tuple(sl)
-                    current = None
+    def run(self):
+        text = None
+        while text != "q":
+            text = input("New message: ")
+            if text != "q":
+                result = self.__predict(text)
+                print('Intent: ', result['intent']['name'])
+                print('Confidence: ', result['intent']['confidence'])
+                if len(result['slots']) > 0:
+                    print('Slots:')
+                    for slot in result['slots']:
+                        print(' ', slot['slot'], '=', slot['value'])
                 else:
-                    end = i
-            previous = current
-        if previous is not None:
-            add(previous, start, end)
-        return slots
+                    print('Slots:\n', ' None')
 
-    def predict(self, utterance):
+    def __predict(self, utterance):
         tokens = utterance.split()
         input_ids, input_mask, segment_ids, valid_positions, data_sequence_lengths = \
             self.bert_vectorizer.transform([utterance])
@@ -66,7 +41,7 @@ class ActionClassifier:
             [input_ids, input_mask, segment_ids, valid_positions],
             self.tags_vectorizer, self.intents_label_encoder, remove_start_end=True,
             include_intent_prob=True)
-        slots = self.convert_to_slots(predicted_tags[0])
+        slots = self.__fill_slots(predicted_tags[0])
         slots = [{"slot": slot, "start": start, "end": end, "value": ' '.join(tokens[start:end + 1])}
                  for slot, start, end in slots]
         predictions = {
@@ -78,10 +53,40 @@ class ActionClassifier:
         }
         return predictions
 
-    def run(self):
-        text = None
-        while text != "q":
-            text = input("New message: ")
-            if text != "q":
-                result = self.predict(text)
-                print(result)
+    def __fill_slots(self, slots_arr, no_class_tag='O', begin_prefix='B-', in_prefix='I-'):
+        previous = None
+        slots = []
+        start = -1
+        end = -1
+        for i, slot in enumerate(slots_arr):
+            if slot == no_class_tag:
+                current = None
+                if previous is not None:
+                    slots.append(self.__form_slot(previous, start, end))
+            if slot.startswith(begin_prefix):
+                current = slot[len(begin_prefix):]
+                if previous is not None:
+                    slots.append(self.__form_slot(previous, start, end))
+                start = i
+            elif slot.startswith(in_prefix):
+                current = slot[len(in_prefix):]
+                if current != previous:
+                    if previous is not None:
+                        slots.append(self.__form_slot(previous, start, end))
+                    for j, sl in enumerate(slots):
+                        if current == sl[0]:
+                            sl = list(sl)
+                            sl[2] = i
+                            slots[j] = tuple(sl)
+                    current = None
+                else:
+                    end = i
+            previous = current
+        if previous is not None:
+            slots.append(self.__form_slot(previous, start, end))
+        return slots
+
+    def __form_slot(self, name, start, end):
+        if end < start:
+            end = start
+        return (name, start, end)
