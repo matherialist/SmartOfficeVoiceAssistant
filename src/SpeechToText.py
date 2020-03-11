d1fe590ae7543f6aa6cba272f67906bb5d0dc187
matherialist <lagrange@phystech.edu>
2020-03-11 21:52:32 +0300

    init method

--- src/SpeechToText.py
+++ src/SpeechToText.py
@@ -2,21 +2,28 @@ import speech_recognition as sr
 
 
 class SpeechToText:
+
+    def __init__(self):
+        self.rec = sr.Recognizer()
+        self.rec.dynamic_energy_threshold = True
+        self.rec.dynamic_energy_adjustment_damping = 0.15
+        # minimum length of silence (in seconds) that will register as the end of a phrase
+        self.rec.pause_threshold = 0.8
+        print('Silence!')
+        with sr.Microphone() as source:
+            self.rec.adjust_for_ambient_noise(source, 3)
+
     def speechRecognition(self):
         # obtain audio from the microphone
-        r = sr.Recognizer()
         with sr.Microphone() as source:
             print("Say something!")
-            audio = r.listen(source)
+            audio = self.r.listen(source)
 
         # recognize speech using Google Speech Recognition
         try:
-            # for testing purposes, we're just using the default API key
-            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-            # instead of `r.recognize_google(audio)`
-            print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+            print("Google Speech Recognition thinks you said: " + self.rec.recognize_google(audio))
         except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
+            print("Google Speech Recognition could not understand audio.")
         except sr.RequestError as e:
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
-        return r.recognize_google(audio)
+        return self.rec.recognize_google(audio)
