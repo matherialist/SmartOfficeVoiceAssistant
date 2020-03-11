62f43ac900a746e45d24f127d5f6465ed437e2b1
kotelnikovaeliz245 <kotelnikovaeliz@yandex.ru>
2020-03-11 11:17:25 +0300

    Realised Text-To-Speech and STT Projects

---
+++ src/TextToSpeech.py
@@ -0,0 +1,45 @@
+import subprocess
+import logging
+import os
+import time
+from mutagen.mp3 import MP3
+from gtts import gTTS
+from datetime import datetime
+
+class TextToSpeech:
+    def __init__(self, playerPath):
+        self.__playerPath = playerPath
+        self.__logPath = None
+        self.__curHash = None
+
+    def voiceText(self, text, lang):
+        self.__curHash = self.__genrateHash()
+        self.__logData("{ \"text\": \"%s\", \"lang\": \"%s\"}" % (text, lang))
+        self.__logData("%s\t%s\tGet" % (datetime.now().isoformat(), self.__curHash))
+        file = self.__genVoice(text, lang)
+        self.__logData("%s\t%s\tCreated" % (datetime.now().isoformat(), self.__curHash))
+        openSubprocess = subprocess.Popen(self.__playerPath)
+        os.startfile(file.filename)
+        self.__logData("%s\t%s\tPlayed" % (datetime.now().isoformat(), self.__curHash))
+        time.sleep(file.info.length)
+        # play mp3
+        openSubprocess.kill()
+        # delete mp3
+        self.__logData("%s\t%s\tDeleted" % (datetime.now().isoformat(), self.__curHash))
+        os.remove(file.filename)
+        # self.__deleteLog()
+
+    def __genrateHash(self):
+        intHash = abs(hash(time.ctime()))
+        hexHash = '{:X}'.format(intHash)
+        return hexHash
+
+    def __genVoice(self, text, lang):
+        tts = gTTS(text, lang=lang)
+        fileName = "%s.mp3" % self.__curHash
+        tts.save(fileName)
+        return MP3(fileName)
+
+    def __logData(self, log):
+        logging.basicConfig(format="", filename="sample.log", level=logging.INFO)
+        logging.info(log)
